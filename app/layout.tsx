d2ccea1ab908b8ade709837a1410a61ad4f5d931
sarrahman-me <sarrahman.me@gmail.com>
2023-10-11 16:17:52 +0700

    :chart_with_upwards_trend: vercel analystic

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 "use client";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
 import store from "@/redux/store";
 import { Provider } from "react-redux";
 import { GoogleAnalytics } from "@/functions";
@@ -11,12 +12,16 @@ export default function RootLayout({
 }) {
   return (
     <html lang="id">
+      <head>
+        <title>Tokokeramik.com - Platform bisnis bahan bangunan</title>
+      </head>
       <body id="root">
         <GoogleAnalytics
           GA_TRACKING_ID={`${process.env.NEXT_PUBLIC_GOOGLE_ANALYSTIC_ID}`}
         />
         <div className="bg-slate-50 dark:bg-slate-900 dark:text-white text-black min-h-screen">
           <Provider store={store}>{children}</Provider>
+          <Analytics />
         </div>
       </body>
     </html>
