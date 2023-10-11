de0609a4d32094ed2120bd9cd10b52950c79e144
sarrahman-me <sarrahman.me@gmail.com>
2023-10-11 15:56:37 +0700

    :bug: salah penempatan pelacakan analystic

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,8 +11,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang="id">
-      <GoogleAnalytics GA_TRACKING_ID="G-DFLVLS41N0" />
       <body id="root">
+        <GoogleAnalytics GA_TRACKING_ID="G-DFLVLS41N0" />
         <div className="bg-slate-50 dark:bg-slate-900 dark:text-white text-black min-h-screen">
           <Provider store={store}>{children}</Provider>
         </div>
