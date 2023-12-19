50f58457dc91ab272b9c16881607566196477261
sarrahman-me <sarrahman.me@gmail.com>
2023-12-19 10:10:02 +0800

    :hammer: mengunci halaman visualisasi

--- app/dashboard/visualisasi/page.tsx
+++ app/dashboard/visualisasi/page.tsx
@@ -1,12 +1,33 @@
 "use client";
+import { ExpiredPlan, NotMembership, PaymentChecking } from "@/src/components";
+import moment from "moment";
 import React from "react";
+import { useSelector } from "react-redux";
 
 export default function Visualisasi() {
+  const { profile, transaksi, membership } = useSelector(
+    (state: any) => state.profile
+  );
   const iframeStyle = {
     width: "100%",
     height: "100vh",
     border: "none",
   };
+
+  if (!profile?.id_membership) {
+    return <NotMembership />;
+  }
+
+  const endDate = moment(Number(membership?.endDate));
+  const isMembershipExpired = endDate.isSameOrBefore(moment(), "day");
+
+  if (isMembershipExpired) {
+    return <ExpiredPlan id_membership={profile.id_membership} />;
+  }
+
+  if (!transaksi?.verifikasi) {
+    return <PaymentChecking />;
+  }
 
   return (
     <div>
