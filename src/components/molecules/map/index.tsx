124f455ed2778803a3f5381061e57bc092dbed09
sarrahman-me <sarrahman.me@gmail.com>
2024-01-10 09:18:20 +0700

    :hammer: memperbaiki tampilan map

--- src/components/molecules/map/index.tsx
+++ src/components/molecules/map/index.tsx
@@ -6,10 +6,19 @@ import {
   Marker,
   Popup,
   useMapEvents,
+  Polyline,
 } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 
 const Map = ({ lokasi }: any) => {
+  const bounds =
+    lokasi.asal.latlang && lokasi.tujuan.latlang
+      ? [
+          lokasi.asal.latlang.split(",").map(parseFloat),
+          lokasi.tujuan.latlang.split(",").map(parseFloat),
+        ]
+      : undefined;
+
   const ChangeMapView = ({ coords }: any) => {
     const map = useMapEvents({
       zoomend: () => {
@@ -22,19 +31,24 @@ const Map = ({ lokasi }: any) => {
 
   return (
     <div style={{ height: "400px" }}>
-      <MapContainer center={[0, 0]} zoom={13} style={{ height: "100%" }}>
+      <MapContainer
+        center={[0, 0]}
+        zoom={13}
+        style={{ height: "100%" }}
+        bounds={bounds}
+      >
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution="Map data © OpenStreetMap contributors"
         />
         {lokasi.asal.latlang && (
           <Marker position={lokasi.asal.latlang.split(",").map(parseFloat)}>
-            <Popup>Asal</Popup>
+            <Popup>{lokasi.asal.nama || "asal"}</Popup>
           </Marker>
         )}
         {lokasi.tujuan.latlang && (
           <Marker position={lokasi.tujuan.latlang.split(",").map(parseFloat)}>
-            <Popup>Tujuan</Popup>
+            <Popup>{lokasi.tujuan.nama || "tujuan"}</Popup>
           </Marker>
         )}
         {lokasi.asal.latlang && lokasi.tujuan.latlang && (
@@ -42,7 +56,15 @@ const Map = ({ lokasi }: any) => {
             coords={lokasi.asal.latlang.split(",").map(parseFloat)}
           />
         )}
-        {/* Implement Leaflet Routing Machine here if needed */}
+        {lokasi.asal.latlang && lokasi.tujuan.latlang && (
+          <Polyline
+            pathOptions={{ color: "#3949AB" }} // Ubah warna Polyline sesuai keinginan Anda
+            positions={[
+              lokasi.asal.latlang.split(",").map(parseFloat),
+              lokasi.tujuan.latlang.split(",").map(parseFloat),
+            ]}
+          />
+        )}
       </MapContainer>
     </div>
   );
