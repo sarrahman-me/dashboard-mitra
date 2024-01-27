59993ed1ffa16d1f9ea38e77aac7e9144f9b3624
sarrahman-me <sarrahman.me@gmail.com>
2024-01-27 08:53:30 +0700

    :hammer: menambahkan voucher untuk renew

--- app/dashboard/membership/renew/page.tsx
+++ app/dashboard/membership/renew/page.tsx
@@ -1,30 +1,22 @@
 "use client";
 import moment from "moment";
-import React, { useEffect, useState } from "react";
-import { GetDataApi, PostDataApi, formatCurrency } from "@/utils";
+import React, { useState } from "react";
+import { PostDataApi, formatCurrency } from "@/utils";
 import { useRouter } from "next/navigation";
 import { HeaderAndBackIcon } from "@/components/molecules";
 import { Confirm, Loading, Notify } from "notiflix";
 import { useSelector } from "react-redux";
-import { Button, ListData } from "@/src/components";
+import { Button, ListData, Textfield } from "@/src/components";
 
 export default function Renew() {
   const router = useRouter();
-  const [membershipPlan, setPlan] = useState({} as any);
-  const { profile, membership } = useSelector((state: any) => state.profile);
-
-  // Mengambil params dari halaman sebelumnya
-  const klasifikasiMembership = membership.klasifikasi;
-
-  useEffect(() => {
-    async function fetchData() {
-      const data = await GetDataApi(
-        `${process.env.NEXT_PUBLIC_HOST}/membership/klasifikasi/${klasifikasiMembership}`
-      );
-      setPlan(data?.data || []);
-    }
-    fetchData();
-  }, [klasifikasiMembership, profile.id_membership, router]);
+  const [vocher, setVocher] = useState("");
+  const [resultRedemVocher, setResultRedemVocher] = useState("");
+  const [discountAmount, setDiscountAmount] = useState(0);
+  const [finalPrice, setFinalPrice] = useState(0);
+  const { profile, klasifikasi_membership } = useSelector(
+    (state: any) => state.profile
+  );
 
   const handlePerbaruiMembership = () => {
     Confirm.show(
@@ -38,6 +30,7 @@ export default function Renew() {
           `${process.env.NEXT_PUBLIC_HOST}/membership/perbarui`,
           {
             id_membership: profile.id_membership,
+            discountAmount,
           }
         );
 
@@ -53,6 +46,32 @@ export default function Renew() {
     );
   };
 
+  const handleRedemVocher = async () => {
+    Loading.circle();
+    const voucherRegex = /^[A-Za-z0-9]{3}-[A-Za-z0-9]{3}-[A-Za-z0-9]{3}$/;
+
+    if (voucherRegex.test(vocher)) {
+      const response = await PostDataApi(
+        `${process.env.NEXT_PUBLIC_HOST}/finance/voucher/check`,
+        { code: vocher, category: "perpanjangan" }
+      );
+      if (response.success) {
+        const diskon =
+          (klasifikasi_membership.harga * response.data.discount_percentage) /
+          100;
+        setDiscountAmount(diskon);
+        setFinalPrice(klasifikasi_membership.harga - diskon);
+        setResultRedemVocher(response.message);
+      } else {
+        setResultRedemVocher(response.message);
+      }
+      Loading.remove();
+    } else {
+      setResultRedemVocher("Format voucer tidak valid");
+      Loading.remove();
+    }
+  };
+
   return (
     <div>
       <HeaderAndBackIcon title={"Halaman pembayaran"} />
@@ -62,22 +81,53 @@ export default function Renew() {
             <p className="text-lg mb-4">Informasi Paket Membership</p>
             <ListData
               label={"Nama Klasifikasi"}
-              value={membershipPlan.nama_klasifikasi}
+              value={klasifikasi_membership.nama_klasifikasi}
             />
             <ListData
               label={"Harga"}
-              value={formatCurrency(membershipPlan.harga || 0)}
+              value={
+                finalPrice !== 0
+                  ? formatCurrency(finalPrice)
+                  : formatCurrency(klasifikasi_membership.harga)
+              }
             />
+            {discountAmount > 0 && (
+              <ListData
+                label={"kamu menghemat"}
+                value={formatCurrency(discountAmount)}
+              />
+            )}
             <ListData
               label={"Tanggal berakhir"}
               value={`${moment().add(1, "month").format("LL")} (1 bulan)`}
             />
+            <p className=" my-2">Vocher (jika ada)</p>
+            <div className="flex items-center">
+              <Textfield
+                placeholder="###-###-###"
+                name={"vocher"}
+                onChange={(value) => setVocher(value)}
+              />
+              <Button
+                disabled={!vocher}
+                onClick={handleRedemVocher}
+                size="large"
+                variant="text"
+              >
+                Gunakan
+              </Button>
+            </div>
+            <p className="text-xs text-indigo-500 md:text-sm">
+              {resultRedemVocher}
+            </p>
           </div>
           <div className="bg-white dark:bg-slate-800 rounded shadow p-6">
             <p className="text-lg mb-4">Instruksi Pembayaran</p>
             <p>Silakan lakukan pembayaran ke rekening berikut:</p>
             <p className="font-semibold mt-2">
-              {formatCurrency(membershipPlan.harga || 0)}
+              {finalPrice !== 0
+                ? formatCurrency(finalPrice)
+                : formatCurrency(klasifikasi_membership.harga)}
             </p>
             <div className="mt-4">
               <p className="font-semibold">Bank: Bank BCA</p>
