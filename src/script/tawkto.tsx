72accc588098d478385d14fd69a4fc5671771421
sarrahman-me <sarrahman.me@gmail.com>
2024-01-29 17:26:01 +0700

    :hammer: merubah whdget tawkto

--- src/script/tawkto.tsx
+++ src/script/tawkto.tsx
@@ -4,16 +4,12 @@ import Script from "next/script";
 const Tawkto = () => {
   return (
     <>
-      <Script
-        id="tawk-to"
-        strategy="afterInteractive"
-        type="text/javascript"
-      >
+      <Script id="tawk-to" strategy="afterInteractive" type="text/javascript">
         {`var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
         (function(){
         var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
         s1.async=true;
-        s1.src='https://embed.tawk.to/65b778620ff6374032c607b3/1hlaasgj0';
+        s1.src='https://embed.tawk.to/65b77ad20ff6374032c60832/1hlabfid6';
         s1.charset='UTF-8';
         s1.setAttribute('crossorigin','*');
         s0.parentNode.insertBefore(s1,s0);
